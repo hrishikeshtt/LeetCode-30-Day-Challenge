83087f15375337470ab60a2d574fbf11dfdb0220
Hrishikesh Thattarath <hrishikesh.thattarath@concentrix.com>
2020-11-23 12:47:36 +0530

    Unique morse code words problem completed

---
+++ LeetCode 30 Day Challenge/2020/November/23/UniqueMorseCodeWords.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode_30_Day_Challenge
+{
+    class UniqueMorseCodeWords
+    {
+        // https://leetcode.com/explore/challenge/card/november-leetcoding-challenge/567/week-4-november-22nd-november-28th/3540/
+        // Accepted
+        static void UniqueMorseCodeWordsMain(string[] args)
+        {
+            string[] words = { "gin", "zen", "gig", "msg" };
+            int uniqueMorseCodeRepresentations = UniqueMorseRepresentations(words);
+            Console.ReadKey();
+        }
+
+        public static int UniqueMorseRepresentations(string[] words)
+        {
+            string[] morseCodes = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+            Dictionary<string, int> morseCodeRepresentations = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                StringBuilder wordMorseCode = new StringBuilder();
+                foreach (char letter in word)
+                {
+                    string letterMorseCode = morseCodes[(int)letter - 97];
+                    wordMorseCode.Append(letterMorseCode);
+                }
+                if (morseCodeRepresentations.ContainsKey(wordMorseCode.ToString()))
+                {
+                    morseCodeRepresentations[wordMorseCode.ToString()] = morseCodeRepresentations[wordMorseCode.ToString()] + 1;
+                }
+                else
+                {
+                    morseCodeRepresentations.Add(wordMorseCode.ToString(), 1);
+                }
+
+            }
+            return morseCodeRepresentations.Count;
+        }
+    }
+}
--- LeetCode 30 Day Challenge/Program.cs
+++ LeetCode 30 Day Challenge/Program.cs
@@ -8,54 +8,7 @@ namespace LeetCode_30_Day_Challenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
-            Trie obj = new Trie();
-            obj.Insert("apple");
-            bool param_2 = obj.Search("app");
-            bool param_3 = obj.StartsWith("app");
-
-        }
-    }
-
-    public class Trie
-    {
-        static readonly int ALPHABET_SIZE = 26;
-        public class TrieNode
-        {
-            public TrieNode[] Children = new TrieNode[ALPHABET_SIZE];
-            public bool IsEndOfWord { get; set; }
-            public TrieNode()
-            {
-                IsEndOfWord = false;
-                for (int i = 0; i < ALPHABET_SIZE; i++)
-                    Children[i] = null;
-            }
-        }
-
-        static TrieNode root = new TrieNode();
-        /** Inserts a word into the trie. */
-        public void Insert(string word)
-        {
-            TrieNode parent = root;
-            for (int i = 0; i < word.Length; i++)
-            {
-                int childIndex = word[i] - 'a';
-                if (parent.Children[childIndex] == null)
-                    parent.Children[childIndex] = new TrieNode();
-                parent = parent.Children[childIndex];
-            }
-            parent.IsEndOfWord = true;
-        }
-        /** Returns if the word is in the trie. */
-        public bool Search(string word)
-        {
-            throw new NotImplementedException();
-        }
-
-        /** Returns if there is any word in the trie that starts with the given prefix. */
-        public bool StartsWith(string prefix)
-        {
-            throw new NotImplementedException();
+            Console.ReadKey();
         }
     }
 }
